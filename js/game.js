51dc38d588a7329523099793282b860043a63c39
Franco Di Rosa <fdr.2013@hotmail.com>
2020-11-06 09:19:28 -0300

    1.1 Drawing in Canvas

--- js/game.js
+++ js/game.js
@@ -3,20 +3,15 @@
 var canvas = null,
     ctx = null;
 
-function paint1(ctx) {
+function paint(ctx) {
     ctx.fillStyle = '#0f0';        // fill color
     ctx.fillRect(50, 50, 100, 60); // fillRect(x,y,width,height)
-}
-
-function paint2(ctx) {
-    ctx.fillStyle = '#0f0';
-    ctx.fillRect(50, 50, 100, 60);
 }
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    paint2(ctx);
+    paint(ctx);
 }
 
 window.addEventListener('load', init, false);
